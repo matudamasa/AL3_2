d5f0ec2fcea8b2193058a46c6793abb81a9403a2
MasatuguMatuda <k021g1292@g.neec.ac.jp>
2022-05-27 15:04:26 +0900

    02_04_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -15,6 +15,22 @@
 /// ゲームシーン
 /// </summary>
 class GameScene {
+  public:
+	  // パーツID
+	  enum PartId {
+		  Root,		// 大元
+		  Spine,	// 脊髄
+		  Chest,	// 胸
+		  Head,		// 頭
+		  ArmL,		// 左腕
+		  ArmR,		// 右腕
+		  Hip,		// 尻
+		  LegL,		// 左足
+		  LegR,		// 右足
+	};
+
+
+
 
   public: // メンバ関数
 	/// <summary>
